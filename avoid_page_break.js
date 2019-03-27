8e9dc5288c81098c981540c96a3d3ebd506e1516
Sulaiman Sanaullah <ss7@illinois.edu>
2019-03-27 15:25:59 -0500

    edit weird indenting

--- avoid_page_break.js
+++ avoid_page_break.js
@@ -1,15 +1,15 @@
 var wrapSiblings = function(root, className, nodeList, siblingNodeList) {
 	
-	for (let i = 0; i < nodeList.length; i++) {
-		let node          = nodeList[i];
-		let siblingNode   = siblingNodeList[i];
-		let wrapper       = document.createElement('div');
-		wrapper.className = className;
+    for (let i = 0; i < nodeList.length; i++) {
+        let node          = nodeList[i];
+	let siblingNode   = siblingNodeList[i];
+	let wrapper       = document.createElement('div');
+	wrapper.className = className;
 		
-		root.insertBefore(wrapper, node);
-		wrapper.appendChild(node);
-		wrapper.appendChild(siblingNode);
-	}
+	root.insertBefore(wrapper, node);
+	wrapper.appendChild(node);
+	wrapper.appendChild(siblingNode);
+    }
 }
 
 var main             = document.querySelector('[role=main]');
